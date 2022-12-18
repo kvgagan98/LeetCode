73d5357e0a6905d8b32ad230bcb1c7d0306aa207
Gagan <kvgagan98@gmail.com>
2022-12-17 17:38:45 -0800

    Added dir for Problem-724

---
+++ Problem-724/solution.c
@@ -0,0 +1,15 @@
+/* Problem 724 - Find Pivot Index */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int pivotIndex(int* nums, int numsSize);
+int main (void)
+{
+    return 0;
+}
+
+int pivotIndex(int* nums, int numsSize)
+{
+    return 0;
+}
