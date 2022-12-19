651cf09cae5de6693a85d5165a6a3dcfbe5badc1
Gagan <kvgagan98@gmail.com>
2022-12-18 17:27:19 -0800

    Working solution with minor runtime error

--- Problem-392/solution.c
+++ Problem-392/solution.c
@@ -6,8 +6,8 @@
 bool isSubsequence(char * s, char * t);
 int main (void)
 {
-    char s[] = "ace";
-    char t[] = "abcd";
+    char s[] = "abc";
+    char t[] = "ahbgdc";
     bool answer = false;
 
     answer = isSubsequence(s,t);
@@ -19,14 +19,37 @@ int main (void)
 
 bool isSubsequence(char * s, char * t)
 {
-    for (int i = 0; i < strlen(s); i++)
+    int sLen = strlen(s);
+    char *chr = (char *) malloc(sLen * sizeof(char));
+    int *chrArr = (int *) malloc(sLen * sizeof(int));
+    int chrLen;
+    int i = 0;
+    int prevVal = 0;
+
+    for (i = 0; i < sLen; i++)
     {
         /* '\0' = 0 in int */
-        if (!strchr(t, (int) (*(s + i))))
+        chr = strchr(t, (int) (*(s + i)));
+        printf("chr = %s\n", chr);
+        chrLen = strlen(chr);
+        if (!chr)
         {
             return false;
         }
+        *(chrArr + i) = chrLen;
     }
+    prevVal = *chrArr;
+    for (i = 1; i < sLen; i++)
+    {
+        /* If descending order */
+        if (prevVal < *(chrArr + i))
+        {
+            return false;
+        }
+        prevVal = *(chrArr + i);
+    }
+    free(chr);
+    free(chrArr);
     
     return true;
 }
